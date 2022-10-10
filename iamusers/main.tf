743e2e8d9d30fd01d3e1d181b9f7266e6f648c75
jabanne <71444007+jabanne@users.noreply.github.com>
2022-10-10 17:22:45 -0400

    terraformprogress
    week 6 and 7 Terraform code

---
+++ iamusers/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  cloud {
+    organization = "JulieTest"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = var.team
+
+  users = [
+    aws_iam_user.user1.name,
+    aws_iam_user.user2.name,
+    aws_iam_user.user3.name
+  ]
+
+  group = aws_iam_group.group.name
+}
+
+resource "aws_iam_group" "group" {
+  name = var.groupname
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "adminpolicy"
+  description = "administrator access"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "*",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "attachadmin" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.policy.arn
+
+}
+
+resource "aws_iam_user" "user1" {
+  name = var.user1
+}
+
+resource "aws_iam_user_login_profile" "user1" {
+  user            = aws_iam_user.user1.name
+  password_length = 8
+}
+
+output "pw1" {
+  value = aws_iam_user_login_profile.user1.password
+}
+
+resource "aws_iam_user" "user2" {
+  name = var.user2
+}
+resource "aws_iam_user_login_profile" "user2" {
+  user            = aws_iam_user.user2.name
+  password_length = 8
+}
+
+output "pw2" {
+  value = aws_iam_user_login_profile.user2.password
+}
+
+resource "aws_iam_user" "user3" {
+  name = var.user3
+}
+resource "aws_iam_user_login_profile" "user3" {
+  user            = aws_iam_user.user3.name
+  password_length = 8
+}
+
+output "pw3" {
+  value = aws_iam_user_login_profile.user3.password
+}
+
+
---
+++ iamusers/variables.tf
@@ -0,0 +1,29 @@
+variable "team" {
+  description = "Team name"
+  type        = string
+  default     = "arpteam"
+}
+
+variable "groupname" {
+  description = "Group name"
+  type        = string
+  default     = "admin"
+}
+
+variable "user1" {
+  description = "User 1 name"
+  type        = string
+  default     = "julie"
+}
+
+variable "user2" {
+  description = "User 2 name"
+  type        = string
+  default     = "hali"
+}
+
+variable "user3" {
+  description = "User 3 name"
+  type        = string
+  default     = "macris"
+}
